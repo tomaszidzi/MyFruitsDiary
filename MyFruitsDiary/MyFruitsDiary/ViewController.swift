57925a759f4a9f8bf30afe09fe7702a33058c19f
Tomasz Idzi <idzitomek@gmail.com>
2020-01-15 18:12:03 +0100

    Initial commit. Create the proejct.

---
+++ MyFruitsDiary/MyFruitsDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFruitsDiary
+//
+//  Created by Tomasz Idzi on 15/01/2020.
+//  Copyright © 2020 Tomasz Idzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
