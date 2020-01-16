6f79ffea818098746e9d59611ff30f4db05db3a2
Tomasz Idzi <idzitomek@gmail.com>
2020-01-16 23:30:19 +0100

    Handle NetworkResponse and implementaion connection and decode for availableFruits and currentEntries.

--- MyFruitsDiary/MyFruitsDiary/Networking/Manager/NetworkManager.swift
+++ MyFruitsDiary/MyFruitsDiary/Networking/Manager/NetworkManager.swift
@@ -6,7 +6,86 @@
 //  Copyright © 2020 Tomasz Idzi. All rights reserved.
 //
 
+import Foundation
+
+enum NetworkResponse: String {
+    case success
+    case badRequest = "Bad request."
+    case failed = "Network request failed."
+    case noData = "Response returned with no data to decode."
+    case unableToDecode = "Can't decode the response."
+}
+
+enum Result<String> {
+    case success
+    case failure (String)
+}
+
 struct NetworkManager {
     static let environment: NetworkEnvironment = .production
     private let router = Router<FruitAPI>()
+    
+    func getFruits(completion: @escaping (_ fruits: [Fruit]?, _ error: String?) -> ()) {
+        router.request(.availableFruits) { data, response, error in
+            if error != nil {
+                completion(nil, "Please check your network connection.")
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                let result = self.handleNetworkResponse(response)
+                switch result {
+                case .success:
+                    guard let responseData = data else { completion(nil, NetworkResponse.noData.rawValue)
+                        return
+                    }
+                    
+                    do {
+                        //let jsonData = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
+                        let apiResponse = try JSONDecoder().decode([Fruit].self, from: responseData)
+                        completion(apiResponse, nil)
+                    } catch {
+                        completion(nil, NetworkResponse.noData.rawValue)
+                    }
+                case .failure(let networkFailureError):
+                    completion(nil, networkFailureError)
+                }
+            }
+        }
+    }
+    
+    func getEntries(completion: @escaping (_ fruits: [FruitList]?, _ error: String?) -> ()) {
+        router.request(.currentEntries) { data, response, error in
+            if error != nil {
+                completion(nil, "Please check your network connection.")
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                let result = self.handleNetworkResponse(response)
+                switch result {
+                case .success:
+                    guard let responseData = data else { completion(nil, NetworkResponse.noData.rawValue)
+                        return
+                    }
+                    
+                    do {
+                        //let jsonData = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
+                        let apiResponse = try JSONDecoder().decode([FruitList].self, from: responseData)
+                        completion(apiResponse, nil)
+                    } catch {
+                        completion(nil, NetworkResponse.noData.rawValue)
+                    }
+                case .failure(let networkFailureError):
+                    completion(nil, networkFailureError)
+                }
+            }
+        }
+    }
+    
+    fileprivate func handleNetworkResponse(_ response: HTTPURLResponse) -> Result<String> {
+        switch response.statusCode {
+        case 200...299: return .success
+        case 501...599: return .failure(NetworkResponse.badRequest.rawValue)
+        default: return .failure(NetworkResponse.failed.rawValue)
+        }
+    }
 }
